5b3679bc043b80d2efecce8c4138d793a59b8c19
mahmoudBM3 <102478649+mahmoudBM3@users.noreply.github.com>
2022-10-13 21:26:12 +0200

    Most USART Settings are now configurable

--- uart.c
+++ uart.c
@@ -1,35 +1,62 @@
-
-
 #include<avr/io.h>
+#include<avr/interrupt.h>
 #include"uart.h"
 #include"common_macros.h"
 UART_ConfigType g_uartconfig;
-
+volatile uint8 g_uartdata = 0;
+void (*g_callbackptr)(void) = NULL_PTR;
+ISR(USART_RXC_vect) {
+	g_uartdata=UDR;
+	if (g_callbackptr != NULL_PTR) {
+		g_callbackptr();
+	}
+}
 void UART_init(UART_ConfigType *config) {
-	g_uartconfig=*config;
+
+	g_uartconfig = *config;
 	/**Enabling Double speed mode*/
 	SET_BIT(UCSRA, U2X);
 	/**Enabling Receiving and Transmitting*/
-	SET_BIT(UCSRB, RXEN);
-	SET_BIT(UCSRB, TXEN);
+	if (config->UART_ReceiveTechnique != NORECEIVE) {
+		SET_BIT(UCSRB, RXEN);
+	} else {
+		CLEAR_BIT(UCSRB, RXEN);
+	}
+	if (config->UART_TransmitTechnique != NOTRANSMIT) {
+		SET_BIT(UCSRB, TXEN);
+	} else {
+		CLEAR_BIT(UCSRB, TXEN);
+	}
 	/**UCSRC init*/
 	SET_BIT(UCSRC, URSEL);
-	/**Selecting Asynchronous mode*/
-	CLEAR_BIT(UCSRC, UMSEL);
-	/**Disabling Parity Error check*/
-	CLEAR_BIT(UCSRC, UPM0);
-	CLEAR_BIT(UCSRC, UPM1);
-	/**Selecting 1 stop bit*/
-	CLEAR_BIT(UCSRC, USBS);
+	/**Selecting Asynchronous or Synchronous mode*/
+	if (config->UART_Mode == ASYNCH) {
+		CLEAR_BIT(UCSRC, UMSEL);
+	} else {
+		SET_BIT(UCSRC, UMSEL);
+	}
+	/**Choosing Parity Error check*/
+	UCSRC = (UCSRC & 0xCF) | ((config->UART_ParityMode & 0x03) << UPM0);
+	/**Selecting stop bit*/
+	if (config->UART_StopBitMode == ONEBIT) {
+		CLEAR_BIT(UCSRC, USBS);
+	} else if (config->UART_StopBitMode == TWOBIT) {
+		SET_BIT(UCSRC, USBS);
+	}
 	/**Selecting Character Size*/
-	CLEAR_BIT(UCSRB, UCSZ2);
-	CLEAR_BIT(UCSRC, UCSZ1);
-	CLEAR_BIT(UCSRC, UCSZ0);
+	UCSRC = (UCSRC & 0xF9) | ((config->UART_CharacterSize & 0x03) << UCSZ0);
 	/**Selecting the BAUD Rate*/
 	uint16 UBRR_value = ((uint32) F_CPU / (8 * config->UART_BaudRate) - 1);
-	UBRRL=UBRR_value;
-	UBRRH=UBRR_value>>8;
+	UBRRL = UBRR_value;
+	UBRRH = UBRR_value >> 8;
+	if (config->UART_ReceiveTechnique == RECEIVEINTERRUPT) {
+		SET_BIT(SREG, 7);
+		SET_BIT(UCSRB, RXCIE);
+	}
 
+}
+void UART_setCallBack(void (*callbackptr)(void)) {
+	g_callbackptr = callbackptr;
 }
 void UART_sendByte(uint8 data) {
 
@@ -39,24 +66,32 @@ void UART_sendByte(uint8 data) {
 	return;
 }
 uint8 UART_receiveByte(void) {
-	while (BIT_IS_CLEAR(UCSRA, RXC))
-		;
-	return UDR;
+	if (g_uartconfig.UART_ReceiveTechnique == RECEIVEPOLLING) {
+		while (BIT_IS_CLEAR(UCSRA, RXC))
+			;
+		return UDR;
+	} else {
+		SET_BIT(SREG, 7);
+		SET_BIT(UCSRB, RXCIE);
+	}
+
+	return 0;
+
 }
-void UART_sendString(char str[]){
-	uint8 count=0;
-	while(str[count]!='#'){
+void UART_sendString(char str[]) {
+	uint8 count = 0;
+	while (str[count] != '#') {
 		UART_sendByte(str[count]);
 		count++;
 	}
 	return;
 }
-void UART_receiveString(char str[]){
-	uint8 count=0;
-		while(str[count]!='#'){
-			str[count]=UART_receiveByte();
-			count++;
-		}
-		str[count]='\0';
-		return;
+void UART_receiveString(char str[]) {
+	uint8 count = 0;
+	while (str[count] != '#') {
+		str[count] = UART_receiveByte();
+		count++;
+	}
+	str[count] = '\0';
+	return;
 }
--- uart.h
+++ uart.h
@@ -9,18 +9,13 @@
 #define UART_H_
 #include"std_types.h"
 typedef struct {
-	enum {
-		NORMAL = 0, DOUBLESPEED = 1
-	} UART_SpeedMode;
-	enum {
-		NOPARITY = 0, EVEN = 2, ODD = 3
-	} UART_ParityMode;
-	enum {
-		FIVEBIT = 0, SIXBIT = 1, SEVENBIT = 2, EIGHTBIT = 3, NINEBIT = 7
-	} UART_CharacterSize;
+	uint32 UART_BaudRate;
 	enum {
 		ASYNCH = 0, SYNCH = 1
 	} UART_Mode;
+	enum {
+		FIVEBIT = 0, SIXBIT = 1, SEVENBIT = 2, EIGHTBIT = 3
+	} UART_CharacterSize;
 	enum {
 		NORECEIVE = 0, RECEIVEPOLLING = 1, RECEIVEINTERRUPT = 2
 	} UART_ReceiveTechnique;
@@ -30,11 +25,15 @@ typedef struct {
 	enum {
 		ONEBIT = 0, TWOBIT = 1
 	} UART_StopBitMode;
-	uint32 UART_BaudRate;
+	enum {
+		NOPARITY = 0, EVEN = 2, ODD = 3
+	} UART_ParityMode;
 
 } UART_ConfigType;
+extern volatile uint8 g_uartdata;
 void UART_init(UART_ConfigType *config);
 void UART_sendByte(uint8 data);
 uint8 UART_receiveByte(void);
+void UART_setCallBack(void (*callbackptr)(void));
 
 #endif /* UART_H_ */
